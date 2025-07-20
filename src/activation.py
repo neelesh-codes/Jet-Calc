3fa3b2900efe91b26f34fc566d1fc2a712653caa
Neelesh Giroh <giroh381@gmail.com>
2025-07-20 13:05:54 +0530

    All things set for activation.py

--- src/activation.py
+++ src/activation.py
@@ -23,21 +23,26 @@ class Activate:
                 "D:\AI Enginerring cource\Jet-Calc\Jet-Calc\\activation.ps1",
             ]
         )
+        quit
+        exit
 
 
 if __name__ == "__main__":
     print("This is the activation file.")
     root = tk.Tk()
-    root.geometry("150x150")
+    root.geometry("400x400")
+    tk.Label(text="After clicking to \"install requirmetns\":").pack()
+    tk.Label(text="Go to the black window that you see ").pack()
+    tk.Label(text="When it says to start the main programm: ").pack()
+    tk.Label(text="Say no").pack()
+    tk.Label(text="Because there is a error causing we are trying to resolve it.").pack()
     install_all = tk.Button(root, text="Install Requirments!!")
-    exit_window = tk.Button(root, text="Exit")
     massage = tk.Label()
     
     install_all.pack()
     massage.pack()
-    exit_window.pack()
 
     install_all.bind("<Button-1>", Activate.install_build_requirments)
-    exit_window.bind("<Button-2>", exit)
+    print("Go back to the main window and close the programm we have sttoped the background files.")
     root.mainloop()
     # Activate.install_build_requirments()
