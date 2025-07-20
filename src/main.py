5aa9397ddb9324bc17b3803adf9fb900ba0918b5
Neelesh Giroh <giroh381@gmail.com>
2025-07-20 13:40:53 +0530

    created GUI backed left

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
-print("Hello world")
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.uix.widget import Widget
+
+Builder.load_file("..\\gui\\main.kv")
+
+class Backend(Widget):
+    pass
+
+
+class JetCalcApp(App):
+    def build(self):
+        return Backend()
+
+if __name__ == "__main__":
+    JetCalcApp().run()
